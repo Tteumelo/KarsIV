04965eba5515b139609e26d315ca540c0d8c54ed
Matheus <matheus1998-1@hotmail.com>
2025-06-18 13:54:50 -0300

    test1

--- App.tsx
+++ App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from './src/types/navigationTypes';
-import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 import CadastroScreen from './src/screens/CadastroScreen';
 import CepErrorScreen from './src/screens/CepErrorScreen';
 import CadastroVeiculoScreen from '@screens/CadastroVeiculoScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import CarDetailsScreen from './src/screens/CarDetailsScreen';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CarDetails" component={CarDetailsScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
