ae91d5778a45d3d02fd76956555969382b7857d4
Matheus de Melo Oliveira <165829581+Tteumelo@users.noreply.github.com>
2025-06-22 22:59:04 -0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,13 +5,13 @@ import { getReactNativePersistence } from 'firebase/auth/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB16pGhSDX2IqFbzGsA5Y6032bgr6Osnjo",
-  authDomain: "karsiv-3645b.firebaseapp.com",
-  projectId: "karsiv-3645b",
-  storageBucket: "karsiv-3645b.appspot.com",
-  messagingSenderId: "877182637116",
-  appId: "1:877182637116:web:c182e6e3f00d1a32f4df39",
-  measurementId: "G-6HK2CG2K0D"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "6",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
